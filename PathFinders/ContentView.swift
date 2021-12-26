2e5a93e0c536f061182fa996fcc71b01ddcfa0f6
milliemince <milliemince4@gmail.com>
2021-12-26 15:47:04 -0600

    Initial Commit

---
+++ PathFinders/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PathFinders
+//
+//  Created by Camille Mince on 12/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PathFinders/PathFindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathFindersApp.swift
+//  PathFinders
+//
+//  Created by Camille Mince on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PathFindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
